83355be6b2d339354cc2ee63e9d6313f92c26a5d
lauernik <lauernik@gmail.com>
2020-02-08 23:17:59 +0500

    Выполнен второй шаг

--- features/page.py
+++ features/page.py
@@ -3,5 +3,6 @@ class SearchPage(object):
         self.browser = brow
         self.browser.get(page)
 
-    def check_field(self):
-        pass
+    def check_field(self, field):
+        self.browser.find_element_by_xpath('//*[@title="'
+                                           + field.lower().title() + '"]')
--- features/steps/website_steps.py
+++ features/steps/website_steps.py
@@ -5,3 +5,8 @@ from page import SearchPage
 @given('зашли на сайт "{page}"')
 def step_impl(context, page):
     context.gogl = SearchPage(context.browser, page)
+
+
+@then('проверили, что появилось поле "{field}"')
+def step_impl(context, field):
+    context.gogl.check_field(field)
