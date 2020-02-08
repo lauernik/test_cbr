4242495e767a21d239daf9fade5404e9e416ce0b
lauernik <lauernik@gmail.com>
2020-02-09 00:50:37 +0500

    Выполнен шаг 3-6

--- features/page.py
+++ features/page.py
@@ -4,5 +4,20 @@ class SearchPage(object):
         self.browser.get(page)
 
     def check_field(self, field):
-        self.browser.find_element_by_xpath('//*[@title="'
-                                           + field.lower().title() + '"]')
+        self.search_field = self.browser.find_element_by_xpath(
+            '//input[@title="' + field + '"]')
+
+    def enter_data_func(self, input_data):
+        self.search_field.send_keys(input_data)
+
+    def click_button(self, button_name):
+        button = self.browser.find_element_by_xpath(
+            '//input[@value="' + button_name + '"]')
+        button.click()
+
+    def find_link(self, link):
+        self.finded_link = self.browser.find_element_by_xpath(
+            '//a[@href="' + link + '"]')
+
+    def click_link(self):
+        self.finded_link.click()
--- features/steps/website_steps.py
+++ features/steps/website_steps.py
@@ -10,3 +10,23 @@ def step_impl(context, page):
 @then('проверили, что появилось поле "{field}"')
 def step_impl(context, field):
     context.gogl.check_field(field)
+
+
+@when('ввели в поле поиск значение "{input_data}"')
+def step_impl(context, input_data):
+    context.gogl.enter_data_func(input_data)
+
+
+@then('нажали на кнопку "{button_name}"')
+def step_impl(context, button_name):
+    context.gogl.click_button(button_name)
+
+
+@when('нашли ссылку "{link}"')
+def step_impl(context, link):
+    context.gogl.find_link(link)
+
+
+@then('нажали на ссылку "http://www.cbr.ru/"')
+def step_impl(context):
+    context.gogl.click_link()
