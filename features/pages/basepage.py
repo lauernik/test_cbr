2647a9363e1bf429002e9d1fd70d4a443b6bde81
lauernik <lauernik@gmail.com>
2020-02-09 15:43:39 +0500

    изменение структуры файлов

---
+++ features/pages/basepage.py
@@ -0,0 +1,16 @@
+class BasePage:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def get_page(self, page):
+        self.browser.get(page)
+
+    def find_element(self, locator):
+        return self.browser.find_element_by_xpath(locator)
+
+    def click_element(self, locator):
+        locator.click()
+
+    def data_input(self, locator, data):
+        locator.send_keys(data)
