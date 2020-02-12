ad5cf26684e9ebda32a0843a97b6e71bbb64b2cc
lauernik <lauernik@gmail.com>
2020-02-12 21:25:22 +0500

    Изменил формат сообщения на человекочитаемый

--- features/send_mail.py
+++ features/send_mail.py
@@ -1,11 +1,13 @@
 import smtplib
 from datetime import datetime
-from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import base64
 from os import listdir
 
 # Для запуска локального SMTP сервера:
 # python3 -m smtpd -n -c DebuggingServer localhost:1025
+# Проверка отображения производилась на сервисе mailtrap.io
 
 
 def send_email(receiver):
@@ -17,11 +19,19 @@ def send_email(receiver):
     message['To'] = receiver
     message['Subject'] = f'website.feature test {time_now}'
 
+    img_tags = ''
     pngfiles = paths_to_png()
-    for file in pngfiles:
-        with open(file, 'rb') as fp:
-            img = MIMEImage(fp.read())
-        message.attach(img)
+    if pngfiles:
+        for file in pngfiles:
+            with open(file, 'rb') as fp:
+                img = base64.b64encode(fp.read()).decode()
+                img_tags += f'<img src="data:image/jpg;base64,{img}">'
+    else:
+        img_tags = '<p>Во время теста что-то пошло не так.</p>'
+
+    html = f"<html><body>{img_tags}</body></html>"
+
+    message.attach(MIMEText(html, 'html'))
 
     with smtplib.SMTP("localhost", 1025) as server:
         server.sendmail(sender, receiver, message.as_string())
